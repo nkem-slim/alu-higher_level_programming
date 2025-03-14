d3d5946edbae54e3d7154d1489f01de18790b391
Nkem Vincent Nweke <vintio234@gmail.com>
2025-03-14 23:32:20 +0200

    SOlution to question 5 --

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
--- Script that prints the full description of the table
--- first_table from the database hbtn_0c_0 in your MySQL server.
-SHOW CREATE TABLE hbtn_0c_0.first_table;
+-- Shows full table info
+-- show create table
+SHOW CREATE TABLE first_table;
