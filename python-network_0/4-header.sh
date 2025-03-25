95d8abec3fa19bd89f5851eb6a7ca457fc88313b
Slim <vintio234@gmail.com>
2025-03-26 00:06:41 +0200

    Solution to question 4 -

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This bash script takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response.
-curl -sH "X-Sxhool-User-Id: 98" "$1"
+# Takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response.
+curl -sH "X-School-User-Id: 98" "$1"
