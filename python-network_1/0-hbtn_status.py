7762a1dbfc375db3256c94c6b2e166b23bdbdd9b
Slim <vintio234@gmail.com>
2025-03-27 00:07:00 +0200

    Solution to question 0 -

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+A Python script that fetches https://alu-intranet.hbtn.io/status
+
+"""
+
+
+import urllib.request
+with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
+    retrieved = response.read()
+
+print("Body response:")
+print("\t- type: {}".format(retrived))
+print("\t- content: {}".format(retrieved))
+print("\t- utf8 content: {}".format(retrieved.decode('utf-8')))
