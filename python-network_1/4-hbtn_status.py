bc0e2fc9245b18ae382ee45a6e3d70609d872b09
Slim <vintio234@gmail.com>
2025-03-27 01:01:02 +0200

    Solution to question 4 -

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-Python script that fetches https://alx-intranet.hbtn.io/status
+Python script that fetches https://alu-intranet.hbtn.io/status
 """
 
 
 if __name__ == "__main__":
     import requests
 
-    retrieved = requests.get('https://0.0.0.0:5050/status')
+    retrieved = requests.get('https://alu-intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(retrieved.text)))
     print("\t- content: {}".format(retrieved.text))
