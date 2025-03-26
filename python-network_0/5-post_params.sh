397bc30c64dfa048b30dc5393e55e66a493743a5
Slim <vintio234@gmail.com>
2025-03-26 22:32:56 +0200

    Solution to question 5 -

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response.
+# Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response.
 curl -d "email=test@gmail.com&name=John&age=30&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
