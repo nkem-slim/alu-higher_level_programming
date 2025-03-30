173f45d79afc33125d4bddcbec0886fb65fcc5ab
Slim <vintio234@gmail.com>
2025-03-30 23:33:01 +0200

    Solution to 5 -

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -5,4 +5,20 @@ Contains one module
 
 
 def text_indentation(text):
-    print(text.replace(". ", ".\n\n").replace("? ", "?\n\n").replace(": ", ":\n\n"))
+    """
+    Prints a text with two new lines after each of these characters: ., ? and :
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    delimiters = ['.', '?', ':']
+    length = len(text)
+
+    output = []
+    for i in range(length):
+        output.append(text[i])
+        if text[i] in delimiters:
+            output.append("\n")
+            print(''.join(output).lstrip())
+            output = []
+    print(''.join(output).lstrip())
