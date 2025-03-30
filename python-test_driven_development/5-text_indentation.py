c3cf7fbb56a8905529fabe9c68d42c815a781673
Slim <vintio234@gmail.com>
2025-03-30 23:21:07 +0200

    Solution to 5 -

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,7 +3,6 @@
 Contains one module
 """
 
-
 def text_indentation(text):
     """
     Prints a text with two new lines after each of these characters: ., ? and :
@@ -11,14 +10,18 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError("text must be a string")
 
-    delimiters = ['.', '?', ':']
-    length = len(text)
-
-    output = []
-    for i in range(length):
-        output.append(text[i])
+    delimiters = {'.', '?', ':'}
+    output = ""
+    i = 0
+    while i < len(text):
+        output += text[i]
         if text[i] in delimiters:
-            output.append("\n")
-            print(''.join(output).strip())
-            output = []
-    print(''.join(output).strip())
+            output += "\n\n"
+            # Skip all spaces after punctuation
+            i += 1
+            while i < len(text) and text[i] == " ":
+                i += 1
+            continue
+        i += 1
+
+    print(output.strip())
