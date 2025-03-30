a59771de47c7ea7b40c831a04d17c3f1547d0088
Slim <vintio234@gmail.com>
2025-03-30 21:15:54 +0200

    Solution to question 9 -

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -4,7 +4,6 @@ Python script that takes your GitHub credentials (username and password)
 and uses the GitHub API to display your id
 """
 
-
 if __name__ == "__main__":
     import requests
     import sys
