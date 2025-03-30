db03692edc0d541683fbc4163796bc0dfeafb92c
Slim <vintio234@gmail.com>
2025-03-30 21:03:05 +0200

    Solution to question 6 -

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """
-Python script  script that takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response.
+Python script  script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response.
 """
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import requests
     import sys
 
