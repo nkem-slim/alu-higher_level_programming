54893d17bf1a7e0f249194bfb3690122dcdb0a40
Slim <vintio234@gmail.com>
2025-03-30 20:55:22 +0200

    Solution to question 6 -

---
+++ python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Python script  script that takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response.
+"""
+
+
+if __name__ == '__main__':
+    import requests
+    import sys
+    
+    url = sys.args[1]
+    email = sys.args[2]
+    data = {'email': email}
+    retrieved = requests.post(url, data)
+    print(retrieved.text)
